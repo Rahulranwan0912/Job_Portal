9b1ed2a208eb5b5c227d3ddba6561b60540bbd76
rahul ranwan <you@example.com>
2023-10-04 13:59:30 +0530

    Online_Job_Portal_master

---
+++ Online_Job_Portal_master/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$user  = "root";
+$password =  "sreelal";
+$database1 = "jobportal";
+$database2 = "location";
+$db1 = new mysqli($host, $user, $password, $database1);
+
+$db2 = new mysqli($host, $user, $password, $database2);
+
+?>
